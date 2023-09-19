95edc28e3e877063718c6234607daf99353cfe2e
phamducai <phamducaihcmut2009@gmail.com>
2023-09-19 09:24:52 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const requester = axios.create({
   baseURL: "https://airbnbnew.cybersoft.edu.vn/",
   headers: {
     TokenCybersoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlanMgMjciLCJIZXRIYW5TdHJpbmciOiIyMi8wNy8yMDIzIiwiSGV0SGFuVGltZSI6IjE2ODk5ODQwMDAwMDAiLCJuYmYiOjE2NzE1NTU2MDAsImV4cCI6MTY5MDEzMTYwMH0.9xF98txLYJ6iM697nwaNKcuHD3IWhsr_8QjnobN_6Cc",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlanMgMjciLCJIZXRIYW5TdHJpbmciOiIyMi8wNy8yMDMwIiwiSGV0SGFuVGltZSI6IjE2ODk5ODQwMDAwMDAiLCJuYmYiOjE2NzE1NTU2MDAsImV4cCI6MTY5MDEzMTYwMH0.m56Vc0hGFMqPmKIb1UEMwT4DAXplkpQTEEH2pCsZWE8",
   },
 });
 
